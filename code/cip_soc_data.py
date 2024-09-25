a6d02678a6d9e40d45c641e89317486709dee341
nenamalikah <nmb72@georgetown.edu>
2024-09-24 20:09:02 -0400

    Update folder structure

---
+++ code/cip_soc_data.py
@@ -0,0 +1,62 @@
+#%%
+import pandas as pd
+
+# CIP Code to SOC: One to many
+
+df = pd.read_csv('../data/cip_university_options.csv')
+print(df.head())
+
+#%%
+
+cip_codes = df['C2023_A.CIP Code -  2020 Classification'].apply(lambda x: (''.join(filter(str.isdigit, x))))
+df['CIP_Code'] = cip_codes
+
+#%%
+soc_crosswalk = pd.read_excel('data/CIP2020_SOC2018_Crosswalk.xlsx', sheet_name='CIP-SOC', converters={'CIP2020Code':str, 'SOC2018Code':str})
+print(soc_crosswalk.head())
+
+#%%
+crosswalk_cip_codes = soc_crosswalk['CIP2020Code'].apply(lambda x: (''.join(filter(str.isdigit, x))))
+
+crosswalk_soc_codes = soc_crosswalk['SOC2018Code'].apply(lambda x: (''.join(filter(str.isdigit, x))))
+soc_crosswalk['CIP_Code'] = crosswalk_cip_codes
+soc_crosswalk['SOC_Code'] = crosswalk_soc_codes
+
+print(soc_crosswalk.head())
+
+
+#%%
+cip_soc_data = df.merge(soc_crosswalk,how='left',on='CIP_Code')
+
+cip_soc_data = cip_soc_data[['unitid', 'institution name', 'year', 'CIP2020Title', 'CIP_Code', 'SOC2018Title', 'SOC_Code']]
+print(cip_soc_data.head())
+
+#%%
+
+cip_desc = pd.read_csv('../data/CIPCode_Descriptions.csv')
+cip_desc.head()
+
+
+cip_desc_codes = cip_desc['CIPCode'].apply(lambda x: (''.join(filter(str.isdigit, x))))
+cip_desc['CIP_Code'] = cip_desc_codes
+
+#%%
+
+data = cip_soc_data.merge(cip_desc[['CIP_Code', 'CIPDefinition','CrossReferences']],how='left',on='CIP_Code')
+data.head()
+
+#%%
+
+ipeds = pd.read_csv('../data/Data_9-3-2024---746.csv')
+ipeds.head()
+
+ipeds = ipeds.iloc[:,0:102]
+
+#%%
+data.rename(columns={'unitid':'UnitID'},inplace=True)
+
+#%%
+data.merge(ipeds,how='inner',on='UnitID') # 627,893
+
+#%%
+data.merge(ipeds,how='left',on='UnitID')
---
+++ progress journal/Progress_Report.md
@@ -0,0 +1,18 @@
+Note: Use Markdown Cheat Sheet if you need more functionality
+https://www.markdownguide.org/cheat-sheet/
+### Date: sep 24 2024 
+- Topics of discussion
+    - Project Report
+    - Project repository in GitHub
+
+![Untitled-2024-09-24-1823.png](..%2F..%2F..%2F..%2F..%2F..%2FDownloads%2FUntitled-2024-09-24-1823.png)
+
+- Action Items:
+
+* [ ] Merge and finalize data for CIP-SOC database (RAG Agent)
+* [ ] Create validation dataset with ChatGPT (RAG Agent)
+* [ ] Generate initial accuracy metrics of RAG Agent with validation dataset
+* [ ] Create component level, test level, and utility level folder structure (parser, unit testing)
+* [ ] Decide vector store and write code for the vector store
+* [ ] Decide which LLM to use and write the code 
+
