361996cc124bcbd634c02f1b40763d60457b2330
nenamalikah <nmb72@georgetown.edu>
2024-10-05 00:29:44 -0400

    Update question-generation.py

--- code/question-generation.py
+++ code/question-generation.py
@@ -1,16 +1,109 @@
 #%%
-from llama_index.core.evaluation import DatasetGenerator, RelevancyEvaluator
-from llama_index.llms.openai import OpenAI
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Response
-
-
-reader = SimpleDirectoryReader(input_files=["./data/CIPCode_Descriptions.csv"])
-documents = reader.load_data()
+from huggingface_hub import login
+login()
 
 #%%
-import openai
-openai.api_key = "*****"
-data_generator = DatasetGenerator.from_documents(documents)
+from langchain_community.document_loaders import CSVLoader
+from tqdm import tqdm
+import pandas as pd
+import random
 
-eval_questions = data_generator.generate_questions_from_nodes()
-eval_questions
+loader = CSVLoader(file_path='./data/final_p1.csv',
+    csv_args={
+    'delimiter': ',',
+    })
+
+docs = []
+docs_lazy = loader.lazy_load()
+
+for doc in tqdm(docs_lazy):
+    docs.append(doc)
+
+#%%
+loader_bls = CSVLoader(file_path='./data/final_p2.csv',
+    csv_args={
+    'delimiter': ',',
+    })
+
+docs_lazy_bls = loader_bls.lazy_load()
+
+for doc in tqdm(docs_lazy_bls):
+    docs.append(doc)
+
+#%%
+from huggingface_hub import InferenceClient
+import json
+
+repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+
+llm_client = InferenceClient(
+    model=repo_id,
+    timeout=120,
+)
+
+def call_llm(inference_client: InferenceClient, prompt: str):
+    response = inference_client.post(
+        json={
+            "inputs": prompt,
+            "parameters": {"max_new_tokens": 1000},
+            "task": "text-generation",
+        },
+    )
+    return json.loads(response.decode())[0]["generated_text"]
+
+
+call_llm(llm_client, "This is a test context")
+
+#%%
+
+QA_generation_prompt = """
+Your task is to write a factoid question and an answer given a context.
+Your factoid question should be answerable with a specific, concise piece of factual information from the context.
+Your factoid question should be formulated in the same style as questions users could ask in a search engine.
+This means that your factoid question MUST NOT mention something like "according to the passage" or "context".
+
+Provide your answer as follows:
+
+Output:::
+Factoid question: (your factoid question)
+Answer: (your answer to the factoid question)
+
+Now here is the context.
+
+Context: {context}\n
+Output:::"""
+
+#%%
+
+N_GENERATIONS = 5  # We intentionally generate only 10 QA couples here for cost and time considerations
+
+print(f"Generating {N_GENERATIONS} QA couples...")
+
+outputs = []
+for sampled_context in tqdm(random.sample(docs, N_GENERATIONS)):
+    # Generate QA couple
+    output_QA_couple = call_llm(llm_client, QA_generation_prompt.format(context=sampled_context.page_content))
+    try:
+        question = output_QA_couple.split("Factoid question: ")[-1].split("Answer: ")[0]
+        answer = output_QA_couple.split("Answer: ")[-1]
+        assert len(answer) < 300, "Answer is too long"
+        outputs.append(
+            {
+                "context": sampled_context.page_content,
+                "question": question,
+                "answer": answer,
+                "source_doc": sampled_context.metadata["source"],
+            }
+        )
+    except:
+        continue
+
+
+print(pd.DataFrame(outputs).head(1))
+
+#%%
+# from langchain_community.vectorstores import DuckDB
+# from langchain_openai import OpenAIEmbeddings
+#
+# embeddings = OpenAIEmbeddings()
+# database = DuckDB.from_documents(docs, embeddings)
