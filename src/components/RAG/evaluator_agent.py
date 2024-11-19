f89c112e5d19643a0bf8441da5cc5da58efaff0f
nenamalikah <nmb72@georgetown.edu>
2024-11-19 12:33:32 -0500

    Update evaluator_agent.py
    Use ResponseSchema object to format llm output

--- src/components/RAG/evaluator_agent.py
+++ src/components/RAG/evaluator_agent.py
@@ -1,26 +1,33 @@
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import JsonOutputParser, PydanticOutputParser
 from langchain_huggingface import HuggingFaceEndpoint
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 
 #%%
 
 def evaluator_agent(question, context, repo_id):
+    response_schemas = [
+        ResponseSchema(name="relevance", description="whether or not the provided context can answer the user query. if it can, the value is generate. otherwise, it is assistant_agent."),
+    ]
+    output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+
     evaluator_template = PromptTemplate(
-        template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a helpful assistant. Given the following context and the user's query, evaluate whether this document contains the relevant information to answer the query. 
-
-    Give a binary score 'yes' or 'no' score to indicate whether the context provides sufficient information to answer the user question. \n
-
-    Provide the binary score as a JSON with a single key 'score' and no premable or explanation. Ensure that the output is in proper JSON format, with **double quotes** for keys and values. 
-     <|eot_id|><|start_header_id|>user<|end_header_id|>
-    Here is the context: \n\n {context} \n\n
-    Here is the user question: {question} \n <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+        template="""You are an expert at evaluating whether provided context can be used to answer a user query. If the context is relevant, you generate an answer, Otherwise, you pass the query to the routing assistant.
+         
+         Based on the user query, return a SINGLE JSON object with a key called 'relevance'. If the provided context provides sufficient information to answer the user question, choose 'generate' as the dictionary value. Otherwise, choose 'assistant_agent'.\n
+         
+         Here is how your response should be formatted: {format_instructions}\n
+         Here is the context: {context}\n
+         Here is the user query: {question}\n
     """,
-        input_variables=["question", "context"])
+        input_variables=["question", "context"],
+                            partial_variables={"format_instructions": output_parser.get_format_instructions()})
 
     llm = HuggingFaceEndpoint(
         repo_id=repo_id
     )
     evaluator_agent = evaluator_template | llm | JsonOutputParser()
+
 
     answer = evaluator_agent.invoke({'question': question, 'context': context})
     return answer
@@ -36,5 +43,4 @@ if __name__ == '__main__':
 
     question = 'How much are the books and supplies at Aaniiih Nakoda College?'
     repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
-
-    evaluator_agent(question=question, context=test_doc, repo_id=repo_id)
+    print(evaluator_agent(question=question, context=test_doc, repo_id=repo_id))
