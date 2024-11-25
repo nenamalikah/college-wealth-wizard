d3b62f9cfe64e8c76c959ab2b1bc0e04d7c71a19
nenamalikah <nmb72@georgetown.edu>
2024-11-25 02:05:28 -0500

    Create app.py

---
+++ src/main/app.py
@@ -0,0 +1,67 @@
+#%%
+from langgraph.graph import END, StateGraph
+
+from typing_extensions import TypedDict
+from typing import Annotated
+from operator import add
+
+import sys
+sys.path.append('../')
+
+from components.RAG.agents import *
+from components.RAG.tools import *
+from components.RAG.helper import *
+
+#%%
+# Create Graph State Object
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        documents: list of documents
+    """
+    question: str
+    generation: str
+    documents: Annotated[list[str], add]
+    sources: Annotated[list[str], add]
+    next_step: str
+#%%
+# Define the nodes
+workflow = StateGraph(GraphState)
+workflow.add_node("vstore_agent", vstore_selection)
+workflow.add_node("evaluator_agent", evaluator)
+workflow.add_node("cs_evaluator", evaluator)
+workflow.add_node("assistant_agent", router_assistant)
+workflow.add_node("cs_api", api_agent)
+workflow.add_node("tavily_search", web_search)
+workflow.add_node("generate", generate_answer)
+
+# Defines the edges
+workflow.set_conditional_entry_point(
+    entry_agent,
+    {"vector store": "vstore_agent", "routing assistant": "assistant_agent"},
+)
+workflow.add_edge("vstore_agent", "evaluator_agent")
+# While at the evaluator agent, generate an answer or go to the router assistant for more information
+workflow.add_conditional_edges("evaluator_agent", vs_evaluation, {'assistant_agent':'assistant_agent',
+                                                               'generate':'generate'}) # do not need nodes for router agents, only the routing functions
+workflow.add_conditional_edges("assistant_agent",choose_secondary_source)
+workflow.add_edge("cs_api","cs_evaluator")
+workflow.add_conditional_edges("cs_evaluator", api_evaluation)
+
+workflow.add_edge("tavily_search", "generate")
+
+workflow.add_edge("generate", END)
+
+# Compile
+graph = workflow.compile()
+
+#%%
+if __name__ == "__main__":
+    question = "What is the average salary of a data scientist?"
+
+    response = graph.invoke({"question": question})
+    print(response)
